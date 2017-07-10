19b84ce365d90de9e4ee8490a751b80856de61bb
Sunil <sunilpdt@gmail.com>
2017-07-10 18:40:27 +0530

    Committing 9.1

---
+++ ACD_JAVAB_Session9_Assignment_1/src/com/acadgild/HashMapExample.java
@@ -0,0 +1,17 @@
+package com.acadgild;
+
+import java.util.HashMap;
+
+public class HashMapExample {
+
+	public static void main(String[] args) {
+		HashMap<Integer, String> hshMp=new HashMap<Integer,String>();
+		hshMp.put(101, "Sunil");
+		hshMp.put(102, "Surendra");
+		
+		System.out.println(hshMp);
+		
+
+	}
+
+}
